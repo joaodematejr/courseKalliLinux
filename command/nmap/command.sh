0e8c974d90b15eca94f1aca92638747ee61862e3
João Dematé Jr <joaodematejr@gmail.com>
2021-01-29 22:17:38 -0300

    ettercap e emap

--- command/nmap/command.sh
+++ command/nmap/command.sh
@@ -4,3 +4,14 @@ nmap -sV -p 80 ip_aqui
 
 
 sudo t50 ip_aqui --dport 80 --flood --syn --turbo 
+
+
+nmap -v -A ip_aqui_roteador
+
+nmap -O ip_aqui_roteador
+
+nmap -PN ip_aqui_roteador
+
+nmap -PA ip_aqui_roteador
+
+nmap -sX ip_aqui_roteador
